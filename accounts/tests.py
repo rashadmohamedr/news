92d0acde9669248cf42faf557d4327db0c2c472e
Rashad El Taher <rashadmohamed2019@icloud.com>
2026-01-30 17:42:23 +0200

    add Bootstrap styling

--- accounts/tests.py
+++ accounts/tests.py
@@ -32,9 +32,12 @@ class UsersManagersTests(TestCase):
     
 class SignupPageTests(TestCase):
     def test_url_exists_at_correct_location_signupview(self):
-        pass
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code,200)
     def test_signup_view_name(self):
-        pass
+        response = self.client.get(reverse("signup"))
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,"registration/signup.html")
     def test_signup_form(self):
         response = self.client.post(
             reverse("signup"),
--- accounts/views.py
+++ accounts/views.py
@@ -5,4 +5,4 @@ from .forms import CustomUserCreationForm
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
-    template_name = "registration/login.html"
+    template_name = "registration/signup.html"
