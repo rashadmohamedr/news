02c9995d510b21183eecfc34a50184a239abace9
Rashad El Taher <rashadmohamed2019@icloud.com>
2026-01-30 15:39:47 +0200

    custom user model

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
+class UsersManagersTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="testuser",
+            email="testuser@email.com",
+            password="testpassword"
+        )
+        self.assertEqual(user.username, "testuser")
+        self.assertEqual(user.email, "testuser@email.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+    
+    def test_create_superuser(self):
+        User = get_user_model()
+        user = User.objects.create_superuser(
+            username="testsuperuser",
+            email="testsuperuser@email.com",
+            password="testsuperpassword"
+        )
+        self.assertEqual(user.username, "testsuperuser")
+        self.assertEqual(user.email, "testsuperuser@email.com")
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+    
+        
